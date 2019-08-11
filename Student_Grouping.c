7c330497bb43eaf25e957b2734536ccfccd002ab
Saviour Code <36518515+saviourcode@users.noreply.github.com>
2019-08-11 21:49:09 +0530

    Add files via upload

---
+++ Student_Grouping.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+void findgroup(int);
+int n;
+int *a;
+
+int main() {
+    printf("Enter the number of students\n");
+    scanf("%d", &n);
+    a = (int*) calloc(n, sizeof (int));
+    printf("Enter the compatiability factors\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &*(a + i));
+    findgroup(n);
+    return 0;
+}
+
+void findgroup(int b) {
+    int c = 0;
+    int numg = 0;
+    int j = 0;
+    for (int i = 0; i < b - 1;) {
+        if (*(a + i) == *(a + 1 + i)) {
+            //printf("In if\n");
+            numg++;
+            i = i + 2;
+        } else if (*(a + i)<*(a + 1 + i)) {
+            c = *(a + i);
+            i++;
+            j = i;
+            while (c != (*(a + j))) {
+                i++;
+                c = c + *(a + i);
+            }
+            numg++;
+            i++;
+        } else if (*(a + i)>*(a + 1 + i)) {
+            j = i;
+            i++;
+            while (c != *(a + j)) {
+                c = c + *(a + i);
+                i++;
+            }
+            numg++;
+        }
+    }
+    printf("Number of Maximum that can be formed = %d\n", numg);
+}
+
