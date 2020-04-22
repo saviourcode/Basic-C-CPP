dff2d9b903e9c9921a0eb79a07adec0ee42e829f
Saviour Code <36518515+saviourcode@users.noreply.github.com>
2020-04-22 17:03:47 +0530

    Update Query_Processing_Input.c

--- Query_Processing_Input.c
+++ Query_Processing_Input.c
@@ -1,27 +1,48 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> //Contains printf and other special characters (\n and EOF) 
+#include <stdlib.h> //For Dynamic Memory allocation, malloc, realloc and free
 
-char* readinput();
+void readline(char**); //readline function prototype
 
+/* Main function */
 int main()
 {
-  char* s;
-  s=readinput();
-  printf("%s",s);
+	/* Pointer to point to the memory location
+	 * where input string will be stored */
+	char *s=NULL;
+	/* Call by reference,the function will place 
+	 * string in memory location where the pointer
+	 *  is pointing */
+	readline(&s); 
+	printf("s=%s\n",s);
+	/* Always free the memory allocated
+	 * to prevent memory leak */
+	free(s);
+	return 0;
 }
 
-char* readinput()
+/* Readline function will
+ * store each input character in
+ * the allocated memory area. */
+void readline(char** p)
 {
-  char* s;
-  char c;
-  int i=0;
-  s=(char*)malloc(1*sizeof(char));
-  while((c=getchar())!='\n' && c!=EOF){
-    s=(char*)realloc(s,(i+1)*sizeof(char));
-    s[i]=c;
-    i++;
-  }
-  s[i]='\0';
-  
-  return s;
+	/* Create dynamic memory if not allocated already
+	 * else use the same memory area and overwrite it */
+	if(*p==NULL){
+		*p=(char*)malloc(1*sizeof(char));
+	}
+
+	char c; //Variable to store each input character
+	int i=0; //Counter to keep track of the size of the input string
+
+	/* Read till newline or end-of-file is not reached
+	 * whichever occurs first. 
+	 * Store the input character into the dynamic memory
+	 * Reallocate the memory if the string is greater than it's 
+	 * initial size */ 
+	while((c=getchar())!='\n' && c!=EOF){
+		*p=(char*)realloc(*p,(i+1)*sizeof(char));
+		*(*p+i)=c;
+		i++;
+	}
+	*(*p+i)='\0'; //Null character to end the string.
 }
