9b88da9afd9b37e8e2eccfca832ff13e2f53f654
Sourabh Choure <36518515+saviourcode@users.noreply.github.com>
2020-07-01 16:35:22 +0000

    Added Copying list function in Level1/linkedlist.c

---
+++ srcs/C/Level1/linkedlist0.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int data;
+    struct node* next;
+} node;
+
+void printll(node* currentPtr)
+{
+    while(currentPtr != NULL){
+        printf("%d  ",currentPtr->data);
+        currentPtr = currentPtr->next;
+    }
+}
+
+void push(node** head,int value)
+{
+    node *newNode = NULL;
+
+    newNode = malloc(1*sizeof(node));
+    newNode->data = value;
+    newNode->next = *head;
+    
+    *head = newNode;
+}
+
+void append(node** head, int value)
+{
+    node *currentPtr = *head;
+    node *newNode = NULL;
+
+    newNode = malloc(1*sizeof(node));
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if(*head == NULL){
+        *head = newNode;
+    }
+
+    else{
+        while(currentPtr->next != NULL){
+            currentPtr = currentPtr->next;
+        }
+        currentPtr->next = newNode;
+    }
+    
+}
+
+node* copylist(node* head)
+{
+    node* current = head;
+    node* newList = NULL;
+    node* tail = NULL;
+
+    while(current != NULL){
+        if(newList == NULL){
+            newList = malloc(1*sizeof(node));
+            newList->data = head->data;
+            newList->next = NULL;
+            tail = newList;
+        }
+        else{
+            tail->next = malloc(1*sizeof(node));
+            tail = tail->next;
+            tail->data = current->data;
+            tail->next = NULL;
+        }
+        current = current->next;
+    }
+
+    return newList;
+}
+int main(void)
+{
+    node* head = NULL;
+    node* head1 = NULL;
+
+    push(&head,40);
+
+    append(&head,1);
+
+    push(&head,35);
+
+    printll(head);
+
+    printf("\nCopying List\n");
+    head1 = copylist(head);
+    printll(head1);
+
+    return 0;
+}
