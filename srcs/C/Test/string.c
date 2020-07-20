115115a453d6c812ea4ff99b57449ab82cb21c5c
Sourabh Choure <36518515+saviourcode@users.noreply.github.com>
2020-07-20 15:08:34 +0000

    Added Test/string.c

---
+++ srcs/C/Test/string.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define BLANK {NULL,0}
+
+typedef struct string{
+    char* str;
+    int size;
+}string;
+
+void append(string* p,char c)
+{
+    if(p->str==NULL){
+        p->str = malloc((p->size+2)*sizeof(char));
+    }
+    else{
+        p->str = realloc(p->str,(p->size+2)*sizeof(char));
+    }
+
+    *(p->str+p->size) = c;
+    *(p->str+p->size+1) = '\0';
+    p->size++;
+}
+
+int main(void)
+{
+    string result = BLANK;
+    char c;
+    printf("Enter the name: ");
+    while((c=getchar())!='\n' && c!=EOF){
+        append(&result,c);
+    }
+
+    printf("result = %s\n",result.str);
+
+    return 0;
+}
