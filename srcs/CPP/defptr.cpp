4fb384290bbf1e983e04d89e62b9fc84b6784933
Sourabh Choure <36518515+saviourcode@users.noreply.github.com>
2020-07-31 17:13:40 +0000

    Add defptr.cpp

---
+++ srcs/CPP/defptr.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class M
+{
+    int x;
+    int y;
+    public:
+        void set_xy(int a,int b)
+        {
+            x = a;
+            y = b;
+        }
+        friend int sum(M m);
+};
+
+int sum(M m)
+{
+    int M::*px = &M::x;
+    int M::*py = &M::y;
+
+    M *pm = &m;
+    int S = m.*px+pm->*py;
+    return S;
+}
+
+int main(void)
+{
+    M n;
+
+    void (M::*pf)(int , int) = &M::set_xy;
+    (n.*pf)(10,20);
+    cout << "SUM = " << sum(n) << endl;
+
+    M *op = &n;
+    (op->*pf)(30,40);
+    cout << "SUM = " << sum(n) << endl;
+
+    return 0;
+}
