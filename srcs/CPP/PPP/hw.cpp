c5a8d157ad8552e50d0c5ca8b28d3c6cb77425f6
Sourabh Choure <36518515+saviourcode@users.noreply.github.com>
2020-08-03 17:53:13 +0000

    Add HW.cpp

---
+++ srcs/CPP/PPP/hw.cpp
@@ -0,0 +1,8 @@
+#include "std_lib_facilities.h"
+
+int main() // C++ programs start by executing the function main
+{
+    cout << "Hello, programming!\nHere we go!\n";
+    keep_window_open();
+    return 0;
+}
