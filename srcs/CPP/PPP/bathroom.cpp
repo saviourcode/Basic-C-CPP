0024ec7da239631824dcb45802b85357fd667778
Sourabh Choure <36518515+saviourcode@users.noreply.github.com>
2020-08-03 17:52:37 +0000

    Add PPP/bathroom.cpp

---
+++ srcs/CPP/PPP/bathroom.cpp
@@ -0,0 +1,15 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+    cout << "Get up from the chair\n" << endl;
+    cout << "Turn left and move 1 step" << endl;
+    cout << "Turn right and move till you reach the door" << endl;
+    cout << "Open the bathroom door by pushing and enter" << endl;
+    cout << "Close the bathroom door and do the X/Y stuff" << endl;
+    cout << "Do all the above steps in reverse order" << endl;
+
+    keep_window_open();
+
+    return 0;
+}
