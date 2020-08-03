3be4d9882731a0e984df6bbe55a7dacb37014354
Sourabh Choure <36518515+saviourcode@users.noreply.github.com>
2020-08-03 17:53:27 +0000

    Initiser.cpp

---
+++ srcs/CPP/PPP/initiser.cpp
@@ -0,0 +1,16 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+    double x{2.7};
+    int y{x};
+    int a{1000};
+    char b{a};
+
+    // double x =2.7;
+    // int y = x;
+    // int a = 1000;
+    // char b = a;
+
+    return 0;
+}
