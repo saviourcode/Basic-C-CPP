2ea1badd9ebdae83e021eca52bd4a3957e051c64
Sourabh Choure <36518515+saviourcode@users.noreply.github.com>
2020-08-05 15:06:58 +0000

    Add e4.cpp

---
+++ srcs/CPP/PPP/chapter3/e4.cpp
@@ -0,0 +1,17 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+    int val1=0;
+    int val2=0;
+
+    cout << "Enter first num: ";
+    cin >> val1;
+    cout << "Enter second num: ";
+    cin >> val2;
+
+    cout << " Sum: " << val1+val2;
+         << " Difference: " << val1-val2;
+         << " Product: " << val1*val2;
+         << " Ratio: " << val1/val2;
+}
