decd97131d7eb79c79cd9a7fbff2c20d1f7197fd
Sourabh Choure <36518515+saviourcode@users.noreply.github.com>
2020-08-06 17:54:38 +0000

    Add seqint.cpp

---
+++ srcs/CPP/PPP/chapter3/seqint.cpp
@@ -0,0 +1,19 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+    cout << "Enter three numbers: ";
+    int arr[3];
+    cin >> arr[0] >> arr[1] >> arr[2];
+
+    for(int j=0;j<3;j++){
+        for(int i=0;i<2;i++){
+            if(arr[i]>arr[i+1]){
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+        }
+    }
+    cout << arr[0] << " " << arr[1] << " " << arr[2] << endl;
+}
